61db4897fa6d07efa8e9fd92195ae89e4a00dab6
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-05-23 20:27:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,7 +16,38 @@ if (localStorage.content != null) {
 }
 
 addBtn.addEventListener("click", function () {
-
+    if (nameInput.value === "" || urlInput.value === "" || nameInput.classList.contains("is-invalid") || urlInput.classList.contains("is-invalid")){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter a name and a URL for the link.',
+        });
+        
+        return;
+    }else{
+        let timerInterval;
+        Swal.fire({
+          title: "The link has been added successfully",
+          html:`link Name <span class="bg-success text-white fw-bold p-2">${nameInput.value}</span>`,
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+              timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          }
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
+    }
     var links = {
         name: nameInput.value,
         url: urlInput.value
@@ -50,13 +81,46 @@ function display() {
 }
 display();
 
-
 function deletItem(i){
-    content.splice(i,1);
-    localStorage.content = JSON.stringify(content);
-    display()
-    location.reload();
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+      });
+      swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          content.splice(i,1);
+          localStorage.content = JSON.stringify(content);
+          display();
+          location.reload();
+
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        } else if (
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            text: "Your imaginary file is safe :)",
+            icon: "error"
+          });
+        }
+      });
 }
+
 
 function clear() {
     nameInput.value=null;
@@ -74,6 +138,38 @@ function updateItem(i) {
     
 }
 updateBtn.addEventListener("click", function () {
+    if (nameInput.value === "" || urlInput.value === "" || nameInput.classList.contains("is-invalid") || urlInput.classList.contains("is-invalid")){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter a name and a URL for the link.',
+        });
+        
+        return;
+    }else{
+        let timerInterval;
+        Swal.fire({
+          title: "The link has been added successfully",
+          html:`link Name <span class="bg-success text-white fw-bold p-2">${nameInput.value}</span>`,
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+              timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          }
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
+    }
 
     addBtn.classList.remove("d-none");
     updateBtn.classList.add("d-none");
@@ -86,11 +182,45 @@ updateBtn.addEventListener("click", function () {
 });
 
 deletaBtn.addEventListener("click",function () {
-    content=[];
-    // content.splice(0);
-    localStorage.clear();
-    display()
-    location.reload();
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+      });
+      swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+            content=[];
+            // content.splice(0);
+            localStorage.clear();
+            display()
+            location.reload();
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            text: "Your imaginary file is safe :)",
+            icon: "error"
+          });
+        }
+      });
+   
 })
 function count() {
     if (content.length > 0) {
@@ -102,22 +232,51 @@ count()
 
 serch.addEventListener("input", function() {
     var term = serch.value;
-    cartona="";
-    for(var i =0;i<content.length;i++){
+    var found = false;
+    cartona = "";
+    for(var i = 0; i < content.length; i++) {
         if (content[i].name.toLowerCase().includes(term.toLowerCase())) {
-          cartona+=`
-          <tr>
-          <th scope="row">${i}</th>
-          <td>${content[i].name}</td>
-  
-          <td><a href="${content[i].url}" target="_blank"><button class="btn btn-info")">view</button></a></td>
-          <td><button class="btn btn-danger" onclick="deletItem(${i})">Delete</button></td>
-          <td><button class="btn btn-success" onclick="updateItem(${i})">Update</button></td>
-          </tr>
-          <tr>
-          `
+            found = true; 
+            cartona += `
+            <tr>
+            <th scope="row">${i}</th>
+            <td>${content[i].name}</td>
+            <td><a href="${content[i].url}" target="_blank"><button class="btn btn-info">View</button></a></td>
+            <td><button class="btn btn-danger" onclick="deletItem(${i})">Delete</button></td>
+            <td><button class="btn btn-success" onclick="updateItem(${i})">Update</button></td>
+            </tr>
+            <tr>`;
         }
-        
+    }
+   
+    if (!found) {
+        cartona = `<div class="alert bg-black position-relative start-100 text-light mt-4 mx-auto" role="alert">
+        <i class="fa-solid fa-face-grin-tongue-squint bg-transparent fs-4"></i>   Not founded!
+                   </div>`;
     }
     document.querySelector("tbody").innerHTML = cartona;
 });
+
+var searchIcon = document.querySelector("#searchIcon");
+
+searchIcon.onclick = function () {
+    serch.classList.toggle("w-100");
+}
+serch.onclick = function () {
+    serch.classList.add("w-100");
+}
+
+function validateForm(e) {
+    var regex = {
+        nameInput : /^[a-z]{4,9}$/,
+        urlInput : /^(https:\/\/)[a-z](.com)$/
+    }
+    if (regex[e.id].test(e.value)) {
+        e.classList.remove("is-invalid");
+        e.classList.add("is-valid");
+        console.log("matcch");
+    }else{
+        e.classList.remove("is-valid");
+        e.classList.add("is-invalid");
+    }
+}
